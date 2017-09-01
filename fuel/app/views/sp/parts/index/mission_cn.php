1ce1369f77a0c36dae55c9a7e7f58ec64a2dc806
Linyang <personal.linyang@gmail.com>
2017-09-02 01:21:00 +0900

    spサイト再修正

--- fuel/app/views/sp/parts/index/mission_cn.php
+++ fuel/app/views/sp/parts/index/mission_cn.php
@@ -1,21 +1,37 @@
-<div class="area-mission">
-	<div class="area-mission-title">
-		<p class="mission-title"><span>Mission</span>　目标</p>
-	</div>
-	<div class="area-mission-content">
-		<p>
-			<span class="sub-title">We create Opportunities to Happiness<br/>
-			我们将提供感受幸福的机会<br/></span><br/>
-			人们对于幸福的定义总是由于周遭的环境差异而有所不同。<br/>
-			在人生中感受到“啊，真是幸福啊”的瞬间多如繁星。<br/>
-			您现在所追求的“幸福”又是什么呢？<br/>
-			“二老身体健康，不必太多担心”<br/>
-			“在能看到漫山樱花的富士山的地方与好友开怀畅饮”<br/>
-			“带上公司的同事拍下可以看成绝境的纪念照片”<br/>
-			等等……<br/><br/>
-			我们作为驻留日本10年的中国人，将会担负起连接中日的桥梁，<br/>
-			注重人与人的联系，在医疗，旅游，乃至商业的方方面面，<br/>
-			为您提供一个感受幸福的机会。<br/>
-		</p>
-	</div>
+<div class="area-maincontent">
+    <div class="area-maincontent-title left">
+        <p class="maincontent-title"><span>Mission</span><br>公司创业理念</p>
+    </div>
+    <div class="area-maincontent-content right">
+        <p>在日本10年，学会了要更加真实和理性的去看待身边的人和物</p>
+        <p>并不是崇洋媚外，但是必须承认日本和国内各自的长处和问题</p>
+        <p>那我们为何不发扬一下我们的优良传统：虚心学习，扬长避短，把好的东西再一次传播回去呢？</p>
+        <p>把我们的所见所闻，把在日本这10年所感受和体验的真实的好的东西通过自己，传达给更多的人，让更多的人可以感受得到</p>
+        <p></p>
+        <p>这就是我们能做的</p>
+        <p></p>
+        <p>所以，就有了O2H。</p>
+    </div>
+</div>
+
+<div class="area-subcontent">
+    <div class="area-subcontent-top"></div>
+    <div class="area-subcontent-content left">
+        <p class="title">O2H：We create an Opportunities(O) to(2) Happiness(H)</p>
+        <p class="subtitle">我们将提供感受幸福的机会</p>
+        <p></p>
+        <p>人们对于幸福的定义总是由于周遭的环境差异而有所不同</p>
+        <p>在人生中感受到“啊，真是幸福啊”的瞬间多如繁星</p>
+        <p>您现在所追求的“幸福”又是什么呢？</p>
+        <p>“二老身体健康，作为儿女不必太多担心”</p>
+        <p>“在能看到漫山樱花的富士山的地方与好友开怀畅饮”</p>
+        <p>“带上亲近的家人和朋友拍下可以看到绝境的纪念照片”</p>
+        <p>等等……</p>
+        <p></p>
+        <p>我们作为驻留日本10年的中国人，想充当起连接中日的桥梁的作用，</p>
+        <p>注重人与人之间的联系，</p>
+        <p>将主要通过在医疗、旅游、婚纱旅行拍摄（旅拍）三大方面乃至其他商业内容，</p>
+        <p>为您提供一个感受幸福的机会</p>
+    </div>
+    <div class="area-subcontent-bottom"></div>
 </div>
--- fuel/app/views/sp/parts/index/mission_ja.php
+++ fuel/app/views/sp/parts/index/mission_ja.php
@@ -1,21 +1,20 @@
-<div class="area-mission">
-	<div class="area-mission-title">
-		<p class="mission-title"><span>Mission</span>　目標</p>
-	</div>
-	<div class="area-mission-content">
-		<p>
-			<span class="sub-title">We create Opportunities to Happiness<br/>
-			僕たちは幸せを感じる機会を提供する<br/></span><br/>
-			人々が幸せに対する定義が周りの環境によって様々です。<br/>
-			人生に「わー、幸せだなー」と感じる瞬間も星ほどあります。<br/>
-			あなたが今追求している【幸せ】は何？<br/>
-			「両親は体が健康で何も心配しなくても良い」<br/>
-			「桜が満開の時期に富士山を見れるところで親友と昼飲み」<br/>
-			「会社の社員や同僚を連れ、絶景の社員旅行の記念撮影」<br/>
-			etc.<br/><br/>
-			僕たちは在日１０年間の中国人として、中国と日本の架け橋となり、<br/>
-			人と人の繋がりを大事にし、医療と旅行だけではなく最終的にビジネスの面でも<br/>
-			一つ幸せを感じる機会を提供していきます。<br/>
-		</p>
-	</div>
+<div class="area-maincontent">
+    <div class="area-maincontent-title left">
+        <p class="maincontent-title"><span>Mission</span><br>目標</p>
+    </div>
+    <div class="area-maincontent-content right">
+        <p class="title">We create Opportunities to Happiness</p>
+        <p class="subtitle">僕たちは幸せを感じる機会を提供する</p>
+        <p>人々が幸せに対する定義が周りの環境によって様々です。</p>
+        <p>人生に「わー、幸せだなー」と感じる瞬間も星ほどあります。</p>
+        <p>あなたが今追求している【幸せ】は何？</p>
+        <p>「両親は体が健康で何も心配しなくても良い」</p>
+        <p>「桜が満開の時期に富士山を見れるところで親友と昼飲み」</p>
+        <p>「会社の社員や同僚を連れ、絶景の社員旅行の記念撮影」</p>
+        <p>etc.</p>
+        <p></p>
+        <p>僕たちは在日１０年間の中国人として、中国と日本の架け橋となり、</p>
+        <p>人と人の繋がりを大事にし、医療と旅行だけではなく最終的にビジネスの面でも</p>
+        <p>一つ幸せを感じる機会を提供していきます。</p>
+    </div>
 </div>
