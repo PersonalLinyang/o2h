a574c744bf5fb0f65f03738b3ef68a16d0d25c89
YangLin <personal.linyang@gmail.com>
2017-12-27 18:18:50 +0900

    SCSS导入

---
+++ public/assets/config.rb
@@ -0,0 +1,20 @@
+Encoding.default_external = "utf-8"
+
+# compassの基準となるディレクトリ
+http_path = "/"
+
+# css用のディレクトリ
+css_dir = "css"
+
+# sass用のディレクトリ
+sass_dir = "scss"
+
+# cssの出力形式
+output_style = :compressed
+
+# css内のパスを相対パスにするかどうか
+relative_assets = true
+
+# コメントを出力するかどうか
+line_comments = false
+
